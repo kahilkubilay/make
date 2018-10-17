628d9b68c6bd36ae52461153717cf7f8d5d38e61
kahilkubilay <kahil_kubilay@hotmail.com>
2018-10-17 14:33:36 +0300

    opacity düzenlendi

--- todo-app-2/script/main.js
+++ todo-app-2/script/main.js
@@ -1,77 +1,71 @@
 window.onload = function(){
 
-    // get reference to element containing toppings checkboxes
-var el = document.getElementById('kontrol');
-
-// get reference to input elements in toppings container element
-var tops = el.getElementsByTagName('input');
-
-// assign function to onclick property of each checkbox
-for (var i=0, len=tops.length; i<len; i++) {
-    if ( tops[i].type == 'checkbox' ) {
-        tops[i].onclick = function() {
-            alert("wwerwerewr");
-        }
-    }
-}
+   
     
     var add_item = document.querySelector("input");
     var button_add_item = document.querySelector("span");
     
-
     add_item.onkeypress = function(add){
         if(add.which == 13){
+            //create element
             var todo_element = document.createElement("LI");
-            var input_element = document.createElement("INPUT");
+            var in_span = document.createElement("SPAN");
+            var in_small = document.createElement("SMALL");
+            //create element input checkbox and value 
+            var in_input = document.createElement("INPUT");
+            in_input.setAttribute("type", "checkbox");
+            in_input.setAttribute("value", "x");
 
+            //create date items
             var date = new Date();
-            var now = date.getDate() + " / " + date.getDate() + " / " + date.getFullYear();
-
-            input_element.setAttribute("type", "checkbox");
-            input_element.setAttribute("value", "x");
-            var item_val = document.createTextNode(this.value);
-            var create_time_item = document.createTextNode(now);
-        
-            //add elements
-            var in_span_val = document.createElement("SPAN");
-            var in_small_val = document.createElement("SMALL");
+            var now = date.getDate() + "/" + date.getDate() + "/" + date.getFullYear();
             
-            in_span_val.appendChild(item_val);
-            in_small_val.appendChild(create_time_item);
+            //in input value
+            
+            var create_time_item = document.createTextNode(now);
+            var item_val = document.createTextNode(this.value);
+            
+            in_small.appendChild(create_time_item);
+            in_span.appendChild(item_val);
+            in_span.appendChild(in_small);
 
-            todo_element.appendChild(input_element);
-            todo_element.appendChild(in_span_val);
-            todo_element.appendChild(in_small_val);
+            //pasta values
+            todo_element.appendChild(in_input);
+            todo_element.appendChild(in_span);
             document.getElementsByClassName("readyItems")[0].appendChild(todo_element);
-        
+
+            //reset input box
             this.value = "";
         }
     }
 
     button_add_item.onclick = function(clicked){
         var todo_element = document.createElement("LI");
-        var item_val = document.createTextNode(add_item.value);
+       
+        var in_span = document.createElement("SPAN");
+        var in_small = document.createElement("SMALL");
         var input_element = document.createElement("INPUT");
+        input_element.setAttribute("type", "checkbox");
+        input_element.setAttribute("value", "x");
 
         var date = new Date();
         var now = date.getDate() + " / " + date.getDate() + " / " + date.getFullYear();
 
-        input_element.setAttribute("type", "checkbox");
-        input_element.setAttribute("value", "x");
+       
         var create_time_item = document.createTextNode(now);
-
+        var item_val = document.createTextNode(add_item.value);
         //add elements
-        var in_span_val = document.createElement("SPAN");
-        var in_small_val = document.createElement("SMALL");
-
-        in_span_val.appendChild(item_val);
-        in_small_val.appendChild(create_time_item);
+       
+        in_small.appendChild(create_time_item);
+        in_span.appendChild(item_val);
+        in_span.appendChild(in_small);
         
+        //paste values
         todo_element.appendChild(input_element);
-        todo_element.appendChild(in_span_val);
-        todo_element.appendChild(in_small_val);
+        todo_element.appendChild(in_span);
         document.getElementsByClassName("readyItems")[0].appendChild(todo_element);
        
+        //reset input
         document.querySelector("input").value = "";
     }
     
